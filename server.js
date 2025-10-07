8c1266cab285f15e7b35d364f3197ac3a53669e2
dianannperla <waafirmelyza@hotmail.com>
2025-10-07 19:50:15 +0100

    Add middleware for JSON parsing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
